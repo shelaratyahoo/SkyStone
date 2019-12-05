48cde82df860ca16174601265173de0dff59df94
shelaratyahoo <shelar@yahoo.com>
2019-12-04 18:45:03 -0600

    Update Test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -12,10 +12,24 @@ public void run0pMode()
 leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
 rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
+drive(motorPower(0.4),1000);
+drive(motorPower(0.2),300);
+drive(motorPower(-0.3),1000);
+
 waitForStart();
 leftDrive.setPower(power);
 rightDrive.setPower(-power);
 
 }
+public double motorPower(double speed) {
+double power = 1*speed;
+return power;
+}
 
+
+
+public void drive(double power, double time) {
+leftDrive.setPower(.5);
+rightDrive.setPower(-.5);
+sleep(time);
 }
